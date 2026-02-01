134b8cbf8b24eaa8078ade608d6566046344bd4c
Kiril Gaydarov <kikchan@users.noreply.github.com>
2026-02-01 14:28:10 +0100

    feature: Added favicon

--- src/ui.js
+++ src/ui.js
@@ -9,6 +9,8 @@ const PORT = process.env.UI_PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 (async () => {
   await initDB();
